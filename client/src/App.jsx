f45b5ad211ce64b61665f1eab37ffd46cd4e3c87
ChetanCoder24 <chetanbarakki24@gmail.com>
2025-10-14 07:32:20 +0530

    FrontendAndBackend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from './components/ui/button'
+import { useEffect, useState } from 'react'
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import SignIn from './pages/signIn'
 import Layout from './layout'
@@ -9,19 +6,42 @@ import EventsPage from './pages/eventsPage'
 import ClubsPage from './pages/clubsPage'
 import Profile from './pages/profile'
 import SignUp from './pages/signUp'
+import { apiClient } from './lib/apiClient'
+import { GETME_ROUTE } from './lib/constants'
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const getUserData = async() => {
+      const response = await apiClient.get(GETME_ROUTE,{withCredentials:true});
+      
+      if(response.status === 200 && response.data.user){
+        setUser(response.data.user);
+        console.log(response.data.user);
+      }else{
+        console.log("NULL");
+      }
+    }
+    getUserData();
+  },[])
+  const PrivateRoute = ({user,children}) => {
+    return user ? children : <Navigate to="/sign-in" />
+  }
+  const AuthRoute = ({user,children}) => {
+    return user ? <Navigate to="/events" /> : children;
+  }
   return (
     <>
     <BrowserRouter>
       <Routes >
-      <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/sign-in" element={<AuthRoute user={user}><SignIn setUserInfo={setUser}/></AuthRoute>}/>
+      <Route path="/sign-up" element={<AuthRoute user={user}><SignUp/></AuthRoute>}/>
         <Route element={<Layout />}>
-          <Route path="/" element={<div>Home page</div>}/>
-          <Route path="/events" element={<EventsPage />}/>
-          <Route path="/clubs" element={<ClubsPage />}/>
-          <Route path="/profile" element={<Profile />}/>
+          <Route path="/" element={<PrivateRoute user={user}>{<div>Home</div>}</PrivateRoute>}/>
+          <Route path="/events" element={<PrivateRoute user={user}><EventsPage /></PrivateRoute>}/>
+          <Route path="/clubs" element={<PrivateRoute user={user}><ClubsPage /></PrivateRoute>}/>
+          <Route path="/profile" element={<PrivateRoute user={user}><Profile /></PrivateRoute>}/>
         </Route>
+        <Route path='*' element={<PrivateRoute user={user}></PrivateRoute>} />
        </Routes>
       </BrowserRouter>
     </>
